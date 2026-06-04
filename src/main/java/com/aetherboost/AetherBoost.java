853b40fe773e2978912d05ae4a7039a4d1338018
RevSkyMC <160483771+ytRevalMC@users.noreply.github.com>
2026-06-04 19:45:07 +0700

    Add files via upload

---
+++ src/main/java/com/aetherboost/AetherBoost.java
@@ -0,0 +1,8 @@
+
+package com.aetherboost;
+import net.minecraftforge.fml.common.Mod;
+@Mod(AetherBoost.MOD_ID)
+public class AetherBoost {
+ public static final String MOD_ID="aetherboost";
+ public AetherBoost(){}
+}
